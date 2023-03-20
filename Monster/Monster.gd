179065b9bdfb9d414b83e8782c72e1b37ae8a0ff
Stephanie Grover <stephanie20@vt.edu>
2023-03-20 19:47:22 -0400

    Removed zombie sprite and added animated monster sprite

--- Monster/Monster.gd
+++ Monster/Monster.gd
@@ -6,10 +6,15 @@ var health = 100
 
 func _physics_process(delta):
 	offset += speed * delta
+	$KinematicBody2D/AnimatedSprite.play("walk")
 	if offset >= 3800:
 		queue_free() ## suppose to mean reached end of path
-
+	
 func hit(damage):
 	health = health - damage
+	$KinematicBody2D/AnimatedSprite.play("Damage Walk")
 	if (health <= 0):
 		queue_free()
+
+
+
