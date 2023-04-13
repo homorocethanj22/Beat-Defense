17472d3b93e54dc132a1d6bec2fc4bfc0a3a5b1b
Joseph Bae <baejoseph@vt.edu>
2023-04-13 13:26:04 -0400

    fixing merge

--- Instruments/SoundWave.gd
+++ Instruments/SoundWave.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 
 var damage = 100
+var textbox_scene = preload("res://Scenes/Textbox.tscn")
 
 func free():
 	queue_free()
@@ -13,4 +14,14 @@ func free():
 
 func _on_SoundWave_body_entered(body):
 	body.get_parent().hit(damage)
+	
+	var textbox = textbox_scene.instance()
+	textbox.position = body.get_global_position() + Vector2(0, 10)
+	print(textbox.position)
+	body.get_parent().add_child(textbox)
+	
+	textbox.set_text("TEMP")
+	
+	textbox.visible = true
+	
 
--- Maps/SceneHandler.gd
+++ Maps/SceneHandler.gd
@@ -1,13 +1,19 @@
 extends Node
 
 func _ready():
-	get_node("MainMenu/MarginContainer/VBoxContainer/NewGame").connect("pressed", self, "on_new_game_pressed")
+	get_node("MainMenu/MarginContainer/VBoxContainer/LevelSelect").connect("pressed", self, "on_level_select_pressed")
+	get_node("MainMenu/MarginContainer/VBoxContainer/Settings").connect("pressed", self, "on_settings_pressed")
+	get_node("MainMenu/MarginContainer/VBoxContainer/About").connect("pressed", self, "on_about_pressed")
 	get_node("MainMenu/MarginContainer/VBoxContainer/Quit").connect("pressed", self, "on_quit_pressed")
 	
-func on_new_game_pressed():
-	get_node("MainMenu").queue_free()
-	var game_scene = load("res://Scenes/GameScene.tscn").instance()
-	add_child(game_scene)
+func on_level_select_pressed():
+	get_tree().change_scene("res://Scenes/LevelSelect.tscn")
+	
+func on_settings_pressed():
+	get_tree().change_scene("res://Scenes/SettingsScene.tscn")
+	
+func on_about_pressed():
+	get_tree().change_scene("res://Scenes/AboutScene.tscn")
 
 func on_quit_pressed():
 	get_tree().quit()
